f60f550e16191f962e6b1363c14530f0a45854a3
Luong Vo <luong.vo@buddify.io>
2018-01-24 16:07:13 +0700

    issue example

---
+++ app/src/main/java/com/github/aleksandermielczarek/androidannotationspermissionsdispatcherpluginexample/ChildMainActivity.java
@@ -0,0 +1,13 @@
+package com.github.aleksandermielczarek.androidannotationspermissionsdispatcherpluginexample;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Copyright © 2017 buddify.io
+ * <p/>
+ * Created by luongvo on 1/24/18.
+ */
+
+@EActivity
+public class ChildMainActivity extends MainActivity {
+}
--- app/src/main/java/com/github/aleksandermielczarek/androidannotationspermissionsdispatcherpluginexample/MainActivity.java
+++ app/src/main/java/com/github/aleksandermielczarek/androidannotationspermissionsdispatcherpluginexample/MainActivity.java
@@ -1,19 +1,12 @@
 package com.github.aleksandermielczarek.androidannotationspermissionsdispatcherpluginexample;
 
 import android.Manifest;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.OnActivityResult;
 
 import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.OnNeverAskAgain;
-import permissions.dispatcher.OnPermissionDenied;
-import permissions.dispatcher.OnShowRationale;
-import permissions.dispatcher.PermissionRequest;
 import permissions.dispatcher.RuntimePermissions;
 
 /**
@@ -23,39 +16,39 @@ import permissions.dispatcher.RuntimePermissions;
 @RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
-    @Click(R.id.permissionButton)
-    protected void askForPermission() {
-        showCamera();
-    }
+//    @Click(R.id.permissionButton)
+//    protected void askForPermission() {
+//        showCamera();
+//    }
 
     @NeedsPermission(Manifest.permission.CAMERA)
     protected void showCamera() {
         Toast.makeText(this, "Permission for camera granted", Toast.LENGTH_SHORT).show();
     }
 
-    @OnPermissionDenied(Manifest.permission.CAMERA)
-    protected void onPermissionDeniedCamera() {
-        Toast.makeText(this, "@OnPermissionDenied for camera", Toast.LENGTH_SHORT).show();
-    }
+//    @OnPermissionDenied(Manifest.permission.CAMERA)
+//    protected void onPermissionDeniedCamera() {
+//        Toast.makeText(this, "@OnPermissionDenied for camera", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @OnShowRationale(Manifest.permission.CAMERA)
+//    protected void showRationaleForCamera(PermissionRequest request) {
+//        Toast.makeText(this, "OnShowRationale for camera", Toast.LENGTH_SHORT).show();
+//        request.proceed();
+//    }
+//
+//    @OnNeverAskAgain(Manifest.permission.CAMERA)
+//    protected void showNeverAskForCamera() {
+//        Toast.makeText(this, "OnNeverAskAgain for camera", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @NeedsPermission(Manifest.permission.WRITE_SETTINGS)
+//    protected void writeSettings() {
+//        Toast.makeText(this, "Permission for write settings granted", Toast.LENGTH_SHORT).show();
+//    }
 
-    @OnShowRationale(Manifest.permission.CAMERA)
-    protected void showRationaleForCamera(PermissionRequest request) {
-        Toast.makeText(this, "OnShowRationale for camera", Toast.LENGTH_SHORT).show();
-        request.proceed();
-    }
-
-    @OnNeverAskAgain(Manifest.permission.CAMERA)
-    protected void showNeverAskForCamera() {
-        Toast.makeText(this, "OnNeverAskAgain for camera", Toast.LENGTH_SHORT).show();
-    }
-
-    @NeedsPermission(Manifest.permission.WRITE_SETTINGS)
-    protected void writeSettings() {
-        Toast.makeText(this, "Permission for write settings granted", Toast.LENGTH_SHORT).show();
-    }
-
-    @OnActivityResult(10)
-    protected void onResult(int resultCode, Intent data) {
-
-    }
+//    @OnActivityResult(10)
+//    protected void onResult(int resultCode, Intent data) {
+//
+//    }
 }
